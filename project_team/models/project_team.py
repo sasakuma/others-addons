162548ab69747d01a16ab209ec1c0d85d9925679
Michell Stuttgart <michellstut@gmail.com>
2017-02-17 17:50:01 -0200

    [NEW] Adicionado metodo para incluir lider do parceiro entre participantes da equipe

--- project_team/models/project_team.py
+++ project_team/models/project_team.py
@@ -5,29 +5,43 @@ from odoo import models, fields, api
 
 
 class CrmTeamInherit(models.Model):
+
     _inherit = 'crm.team'
 
-    type_team = fields.Selection([('sale', 'Sale'), ('project', 'Project')],
+    type_team = fields.Selection([('sale', 'Sale'),
+                                  ('project', 'Project')],
                                  string="Type", default="sale")
 
-    team_members = fields.Many2many('res.users', 'project_team_user_rel',
-                                    'team_id', 'uid', 'Project Members',
+    team_members = fields.Many2many('res.users', string='Project Members',
                                     help="""Project's members are users who
                                      can have an access to the tasks related
                                      to this project.""")
 
+    @api.model
+    def create(self, values):
+        values['team_members'] = [(4, values['user_id'])]
+        return super(CrmTeamInherit, self).create(values)
+
+    @api.multi
+    def write(self, values):
+        if 'user_id' in values:
+            values['team_members'] = [(4, values['user_id'])]
+        return super(CrmTeamInherit, self).write(values)
+
 
 class ProjectProject(models.Model):
+
     _inherit = 'project.project'
 
-    members = fields.Many2many('res.users', 'project_user_rel', 'project_id',
-                               'uid', 'Project Members', help="""Project's
-                               members are users who can have an access to
-                               the tasks related to this project."""
-                               )
     team_id = fields.Many2one('crm.team', string="Project Team",
                               domain=[('type_team', '=', 'project')])
 
-    @api.onchange('team_id')
-    def get_team_members(self):
-        self.members = [(6, 0, [rec.id for rec in self.team_id.team_members])]
+    members = fields.Many2many('res.users', string='Project Members',
+                               related='team_id.team_members',
+                               help="""Project's members are users who can
+                               have an access to the tasks related to this
+                               project.""")
+
+    # @api.onchange('team_id')
+    # def get_team_members(self):
+    #     self.members = [(6, 0, [rec.id for rec in self.team_id.team_members])]
