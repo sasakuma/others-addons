e87567516f5a7ed34602430efbe2e2085ecf0869
rodrigets <rodrigosferreira91@gmail.com>
2017-06-12 10:53:40 -0300

    [REF] Migração do módulo color_field_widget para a versão odoo 10

--- colored_field_widget/__manifest__.py
+++ colored_field_widget/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 {
     'name': 'Custom colored widgets',
-    'version': '0.1',
+    'version': '10.0.1.0.0',
     'summary': 'Some custom widgets',
     'category': 'Tools',
     'author': 'KIT-XXI',
