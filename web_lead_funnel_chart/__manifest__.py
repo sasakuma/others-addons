a15a45a573427f288bb7fae45eb1884386e047ed
Michell Stuttgart <michellstut@gmail.com>
2017-09-01 14:36:19 -0300

    [NEW] Add 'web_lead_funnel_chart' added

---
+++ web_lead_funnel_chart/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Web Lead Funnel Chart 10.0',
+    'category': 'Web',
+    'author': 'Serpent Consulting Services Pvt. Ltd.',
+    'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
+    'summary': 'Funnel Chart for Leads & Opportunities',
+    'website': 'http://www.serpentcs.com',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'description': '',
+    'depends': [
+        'crm'
+    ],
+    'data': [
+        "views/templates.xml",
+        "views/web_lead_funnel_chart_view.xml"
+    ],
+    'qweb': [
+        'static/src/xml/*.xml',
+    ],
+    'images': ['static/description/funnel.jpg'],
+    'installable': True,
+    'auto_install': False,
+}
