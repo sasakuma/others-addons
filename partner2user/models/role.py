89b7a76a5433eab682ba0fa115fee7e24aa4f6ae
Rodrigo Ferreira <rodrigosferreira91@gmail.com>
2018-01-11 16:53:30 -0200

    [FIX] Refatoração de codigo no metodo '_generate_dict_from_record'

--- partner2user/models/role.py
+++ partner2user/models/role.py
@@ -14,52 +14,46 @@ class ResUsersRole(models.Model):
         date_now = time.strftime("%Y-%m-%d %H:%M:%S")
         copy_name = self.name + u' ' + date_now
 
-        group_dict = self._prepare_dict_to_create_backup_record(self.group_id)
+        group_dict = self._generate_dict_from_record(self.group_id)
         group_dict['name'] = copy_name
         group_rec = self.env['res.groups'].create(group_dict)
 
-        role_dict = self._prepare_dict_to_create_backup_record(self)
+        role_dict = self._generate_dict_from_record(self)
         role_dict['name'] = copy_name
         role_dict['group_id'] = group_rec.id
         role_dict['line_ids'] = False
 
-
-        # default['group_id'] = self.env['res.groups'].create(group_dict).id
-        # copy_record = super(ResUsersRole, self).copy(default=default)
-
         return self.env['res.users.role'].create(role_dict)
 
-    def _prepare_dict_to_create_backup_record(self, rec, backup_dict=None):
+    def _generate_dict_from_record(self, rec, record_dict=None):
         """
         :param rec: Record that will be used to examine type of each field
                        to adjust it in case of relational type.
-        :param backup_dict: Base dict containing each field of 'rec' object.
-        :param limit_lvl: How deep the function is called recursively(like
-                          levels in graphs).
-        :return: backup_dict with fields of relational type readjusted.
+        :param record_dict: Base dict containing each field of 'rec' object.
+        :return: record_dict with fields of relational type readjusted.
         """
         if rec:
-            backup_dict = rec.read()[0] if backup_dict is None else backup_dict
+            record_dict = rec.read()[0] if record_dict is None else record_dict
 
             date_fields_to_avoid = ['create_date', 'write_date', 'id',
                                     'create_uid', 'write_uid']
 
             for field in date_fields_to_avoid:
-                if field in backup_dict:
-                    backup_dict.pop(field)
+                if field in record_dict:
+                    record_dict.pop(field)
 
             dic_fields_to_avoid = dict(rec._field_inverses._map,
                                        **rec._field_computed)
 
             for field in dic_fields_to_avoid:
                 if (rec._fields[field.name].type != 'one2many' and
-                            field.name in backup_dict):
-                    backup_dict.pop(field.name)
+                            field.name in record_dict):
+                    record_dict.pop(field.name)
                     continue
 
-            readjusted_dict = dict(backup_dict)
+            readjusted_dict = dict(record_dict)
 
-            for field in backup_dict:
+            for field in record_dict:
                 if not rec[field]:
                     readjusted_dict.pop(field)
                 elif rec._fields[field].type == 'many2one' and rec[field]:
