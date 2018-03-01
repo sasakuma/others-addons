1e2cef824deb38ac1396107bfd0f126a3777152d
Rodrigo Ferreira <rodrigosferreira91@gmail.com>
2018-03-01 15:26:48 -0300

    [MIG] int_range_widget

--- int_range_widget/__manifest__.py
+++ int_range_widget/__manifest__.py
@@ -1,14 +1,13 @@
-# -*- encoding: utf-8 -*-
-
 {
     'name': 'Int Range Widget',
     'summary': 'This project aims to create integer range for input',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Tools',
     'author': "binhnguyenxuan (www.xubi.me)",
     'website': 'http://www.xubi.me',
     'license': 'AGPL-3',
     'depends': [
+        'web',
     ],
     'data': [
         'views/templates.xml',
@@ -16,6 +15,6 @@
     'qweb': [
         "static/src/xml/*.xml",
     ],
-    'installable': False,
+    'installable': True,
     'application': True,
 }
